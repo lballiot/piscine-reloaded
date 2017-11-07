d7aa85339307fe152a56dcc96ad9fef705565e5f
Lydie BALLIOT <lballiot@e2r10p23.42.fr>
2017-11-07 11:36:04 +0100

    Add ft_strcmp.c

--- ex17/ft_strcmp.c
+++ ex17/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: lballiot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 10:59:49 by lballiot          #+#    #+#             */
-/*   Updated: 2017/11/07 11:32:56 by lballiot         ###   ########.fr       */
+/*   Updated: 2017/11/07 11:35:50 by lballiot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,9 @@ int	ft_strcmp(char *s1, char *s2)
 		s2++;
 	}
 	if (*s1 == '\0' && *s2 != '\0')
-		return(-*s2);
+		return (-*s2);
 	else if (*s1 != '\0' && *s2 == '\0')
-		return(*s1 - *s2);
+		return (*s1 - *s2);
 	else
-		return(*s1 - *s2);
-}
-
-#include <string.h>
-
-int main(int arc, char **arv)
-{
-	(void)arc;
-	printf("fct : %d\n", ft_strcmp(arv[1], arv[2]));
-	printf("real : %d\n", strcmp(arv[1], arv[2]));
-	return(0);
+		return (*s1 - *s2);
 }
