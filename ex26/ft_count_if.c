18956bc98ab138e2ace97728e17be7ad05addd2e
getrembl <getrembl@student.42.fr>
2017-11-09 15:38:34 +0100

    add ex 24 ex 26

--- ex26/ft_count_if.c
+++ ex26/ft_count_if.c
@@ -10,38 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int		f(char *str)
 {
-	int i;
-
-	i = 1;
-	return (i);
+	if (*str == 'a')
+		return (1);
+	return (0);
 }
 
 int		ft_count_if(char **tab, int (*f)(char *))
 {
 	int i;
+	int j;
 
 	i = 0;
+	j = 0;
 	while (tab[i] != 0)
-	{
-		f(tab[i++]);
-	}
-	return (i);
+		if (f(tab[i++]) == 1)
+			j++;
+	return (j);
 }
 
 int		main(void)
 {
-	char **tab;
+	char *tab[7];
 
-	tab[0] = "ab";
+	tab[0] = 0;
 	tab[1] = "abbgf";
 	tab[2] = "afdb";
-	tab[3] = "";
+	tab[3] = "0";
 	tab[4] = "afdb";
-	tab[12] = "afdb";
+	tab[5] = "fdb";
 	tab[6] = 0;
 	ft_count_if(tab, &f);
-	printf("%d\n", ft_count_if(tab, &f));
+	printf("main `%d\n", ft_count_if(tab, &f));
 	return (0);
 }
