8ba73a2de09da0685fc9f89f5a00c2170180680b
Lydie BALLIOT <lballiot@e2r7p23.42.fr>
2017-11-09 11:20:51 +0100

    Add ft_foreach.c

--- ex25/ft_foreach.c
+++ ex25/ft_foreach.c
@@ -6,17 +6,9 @@
 /*   By: lballiot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 10:42:06 by lballiot          #+#    #+#             */
-/*   Updated: 2017/11/09 11:19:23 by lballiot         ###   ########.fr       */
+/*   Updated: 2017/11/09 11:20:12 by lballiot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-
-void	f(int i)
-{
-	i = i * 2;
-	printf("i %d\n", i);
-}
 
 void	ft_foreach(int *tab, int length, void (*f)(int))
 {
@@ -25,16 +17,4 @@ void	ft_foreach(int *tab, int length, void (*f)(int))
 	i = 0;
 	while (i < length)
 		f(tab[i++]);
-}
-
-int		main(void)
-{
-	int tab[4];
-
-	tab[0] = 1;
-	tab[1] = 2;
-	tab[2] = 3;
-	tab[3] = 0;
-	ft_foreach(tab, 3, &f);
-	return (0);
 }
